8311c0ab7ecab7afed9244ae5547582318bfe201
hemantseth0210 <hemant.seth02@gmail.com>
2020-12-16 21:52:59 -0500

    Added the exception to the Flux

--- src/test/java/com/seth/java/springreactivestack/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/seth/java/springreactivestack/fluxandmonoplayground/FluxAndMonoTest.java
@@ -7,8 +7,17 @@ public class FluxAndMonoTest {
 
     @Test
     public void fluxTest(){
-
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                //.concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("After Error"))
+                .log();
+        stringFlux
+                .subscribe(System.out::println, (e) -> System.err.println(e), () -> System.out.println("Completed"));
+    }
+
+    @Test
+    public void fluxTest_Exception(){
+    Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
                                         .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
                                         .log();
         stringFlux
