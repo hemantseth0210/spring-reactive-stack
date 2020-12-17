1532ff4a20da68a447b629d354c41ac5539598e3
hemantseth0210 <hemant.seth02@gmail.com>
2020-12-17 00:18:57 -0500

    Implemented the Junit test cases for Flux and Mono factory methods

---
+++ src/test/java/com/seth/java/springreactivestack/fluxandmonoplayground/FluxAndMonoFactoryTest.java
@@ -0,0 +1,75 @@
+package com.seth.java.springreactivestack.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class FluxAndMonoFactoryTest {
+
+    List<String> names = Arrays.asList("Adam", "Anna", "Jack", "Cindy");
+
+    @Test
+    public void fluxUsingIterable(){
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Adam", "Anna", "Jack", "Cindy")
+                .verifyComplete();
+     }
+
+    @Test
+    public void fluxUsingArray(){
+        String[] names = new String[]{"Adam", "Anna", "Jack", "Cindy"};
+        Flux<String> namesFlux = Flux.fromArray(names);
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Adam", "Anna", "Jack", "Cindy")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxUsingStream(){
+        Flux<String> namesFlux = Flux.fromStream(names.stream())
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Adam", "Anna", "Jack", "Cindy")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoUsingJustOrEmpty(){
+        Mono<String> mono = Mono.justOrEmpty(null);
+
+        StepVerifier.create(mono.log())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoUsingSupplier(){
+        Supplier<String> stringSupplier = () -> "Adam";
+        Mono<String> stringMono = Mono.fromSupplier(stringSupplier);
+        StepVerifier.create(stringMono.log())
+                .expectNext("Adam")
+                .verifyComplete();
+
+    }
+
+
+    @Test
+    public void fluxUsingRange(){
+        Flux<Integer> integerFlux = Flux.range(1, 5);
+        StepVerifier.create(integerFlux.log())
+                .expectNext(1,2,3,4,5)
+                .verifyComplete();
+    }
+
+
+
+}
