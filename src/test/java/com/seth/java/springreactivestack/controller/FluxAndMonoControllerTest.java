7ccb92a1c38fcdbf0cfc575f4e47bc08b91f7595
hemantseth0210 <hemant.seth02@gmail.com>
2020-12-17 22:34:18 -0500

    Junit for FluxAndMonoController

--- src/test/java/com/seth/java/springreactivestack/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/seth/java/springreactivestack/controller/FluxAndMonoControllerTest.java
@@ -1,4 +1,71 @@
 package com.seth.java.springreactivestack.controller;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@WebFluxTest
 public class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void flux_approach1(){
+        Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectSubscription()
+                .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void flux_approach2(){
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    public void flux_approach3(){
+        EntityExchangeResult<List<Integer>> listEntityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+        Assertions.assertEquals(Arrays.asList(1,2,3,4), listEntityExchangeResult.getResponseBody());
+    }
+
+    @Test
+    public void flux_approach4(){
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith(response -> {
+                    Assertions.assertEquals(Arrays.asList(1,2,3,4), response.getResponseBody());
+                });
+    }
+
 }
